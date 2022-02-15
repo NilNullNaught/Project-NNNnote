5fc4175cadf025ff70e0ecd359554633c377e540
NNN <null3519768673@gmail.com>
2022-02-15 23:27:56 +0800

    2022/2/15

--- NNNnoteFrontEnd/NNNnoteFront/pages/user/nfolder/index.vue
+++ NNNnoteFrontEnd/NNNnoteFront/pages/user/nfolder/index.vue
@@ -2,26 +2,25 @@
   <div>
     <el-container class="userindex-height">
       <el-main>
+        <!-- 操作栏 ----------------------------------------------------------------------------------------------------------------------------------------->
         <el-row>
           <el-col :span="18">
             <el-button
               type="primary"
-              :disabled="search.isSearching"
               @click="NfolderDialogInitialize('新建文件夹')"
             >
               新建文件夹
             </el-button>
             <el-button-group
-              v-show="checkedList.length !== 0"
+              v-show="select.checkedList.length !== 0"
               ref="userIndex_ebg"
-              :disabled="search.isSearching"
             >
               <el-button type="primary" @click="deleteDialog.visible = true">
                 <i class="el-icon-delete el-icon--left" />
                 删除文件夹
               </el-button>
               <el-button
-                v-show="checkedList.length < 2"
+                v-show="select.checkedList.length < 2"
                 type="primary"
                 @click="NfolderDialogInitialize('编辑文件夹')"
               >
@@ -35,26 +34,29 @@
               v-model="search.searchKeyWord"
               placeholder="请输入内容"
               clearable
-              @change="searchNfolderANDNote()"
+              @change="searchNfolderANDNote"
             />
           </el-col>
         </el-row>
+        <!----------------------------------------------------------------------------------------------------------------------------------------->
 
-        <div style="margin:10px">
+        <!-- 全选框 ----------------------------------------------------------------------------------------------------------------------------------------->
+        <div style="margin: 10px;">
           <el-checkbox
-            v-model="selectAll"
-            :disabled="search.isSearching"
-            :indeterminate="isIndeterminate"
-            :label="selectSum"
+            v-model="select.selectAll"
+            :indeterminate="select.isIndeterminate"
+            :label="select.selectSum"
             @change="handleCheckAll"
           />
         </div>
+        <!----------------------------------------------------------------------------------------------------------------------------------------->
 
         <el-divider />
 
+        <!-- 用户文件夹列表 ----------------------------------------------------------------------------------------------------------------------------------------->
         <el-row v-if="!search.isSearching">
           <el-col v-for="(o) in list" :key="o.id" :span="4">
-            <el-card :id="'no'+o.id" class="userindex-el-card" shadow="hover" style="position: relative;">
+            <el-card :id="'ID-'+o.id" class="userindex-el-card" shadow="hover" style="position: relative;">
               <input
                 v-if="o.id !== o.userId"
                 v-model="o.ischecked"
@@ -62,7 +64,6 @@
                 type="checkbox"
                 @change="handleChecked(o)"
               >
-
               <div @click="route(o.id)">
                 <div style="display: flex;justify-content:center;">
                   <img src="~/assets/img/mine/folder.png" alt>
@@ -74,10 +75,19 @@
             </el-card>
           </el-col>
         </el-row>
+        <!----------------------------------------------------------------------------------------------------------------------------------------->
 
+        <!-- 搜索结果列表 ----------------------------------------------------------------------------------------------------------------------------------------->
         <el-row v-if="search.isSearching">
           <el-col v-for="(so) in search.searchResult" :key="so.id" :span="4">
-            <el-card :id="'sno'+so.id" class="userindex-el-card" shadow="hover" style="position: relative;">
+            <el-card :id="'SID-'+so.id" class="userindex-el-card" shadow="hover" style="position: relative;">
+              <input
+                v-if="so.id !== so.userId"
+                v-model="so.ischecked"
+                style="position:absolute;top: 10px;left: 10px;"
+                type="checkbox"
+                @change="handleChecked(so)"
+              >
               <div @click="route($event,so.id)">
                 <div style="display: flex;justify-content:center;">
                   <img src="~/assets/img/mine/note.png" alt>
@@ -89,8 +99,10 @@
             </el-card>
           </el-col>
         </el-row>
+        <!----------------------------------------------------------------------------------------------------------------------------------------->
       </el-main>
 
+      <!-- 分页 ----------------------------------------------------------------------------------------------------------------------------------->
       <el-footer height="30px">
         <el-row justify="center" type="flex">
           <el-pagination
@@ -110,8 +122,10 @@
           />
         </el-row>
       </el-footer>
+    <!------------------------------------------------------------------------------------------------------------------------------------------------->
     </el-container>
 
+    <!-- 新增与删除文件夹对话框 ----------------------------------------------------------------------------------------------------------------------------------------->
     <el-dialog
       width="30%"
       :title="NfolderDialog.title"
@@ -151,19 +165,22 @@
         <el-button type="primary" @click="submitNfolder()">确认</el-button>
       </span>
     </el-dialog>
+    <!------------------------------------------------------------------------------------------------------------------------------------------------->
 
+    <!-- 删除确认对话框 ----------------------------------------------------------------------------------------------------------------------------------------->
     <el-dialog
       title="提示"
       :visible.sync="deleteDialog.visible"
       width="30%"
       :before-close="handleClose"
     >
-      <span>确认删除{{ checkedList.length }}个文件夹？</span>
+      <span>确认删除{{ select.checkedList.length }}个文件夹？</span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="deleteDialog.visible = false">取 消</el-button>
         <el-button type="primary" @click="deleteUserNFolder">确 定</el-button>
       </span>
     </el-dialog>
+    <!------------------------------------------------------------------------------------------------------------------------------------------------->
   </div>
 </template>
 
@@ -178,10 +195,12 @@ export default {
   data () {
     return {
       list: [],
-      checkedList: [],
-      selectAll: false,
-      isIndeterminate: false,
-      selectSum: '全选',
+      select: {
+        checkedList: [],
+        selectAll: false,
+        isIndeterminate: false,
+        selectSum: '全选'
+      },
       pagination1: {
         current: 1,
         limit: 24,
@@ -232,44 +251,54 @@ export default {
     },
 
     // <- 选中与全选
-    // 选取全部，参数 clear 用于在方法中判断是否需要清空
+    // 选取全部
     handleCheckAll () {
-      if (this.selectAll) {
-        this.checkedList = [...this.list]
-        const hasDefaultFolder = this.checkedList.findIndex(o => o.id === o.userId)
-        if (hasDefaultFolder !== -1) {
-          this.checkedList.splice(hasDefaultFolder, 1)
-        }
-        this.selectSum = '已选中' + this.checkedList.length + '个文件夹'
-      } else {
-        this.checkedList = []
-        this.selectSum = '全选'
-      }
-      this.isIndeterminate = false
+      this.select.isIndeterminate = false
 
-      this.list.forEach((o) => {
-        if (o.id !== o.userId) {
-          o.ischecked = this.selectAll
-          this.addOrRemoveStyle(o.id, this.selectAll)
+      if (this.select.selectAll) {
+        this.select.checkedList = [...this.list]
+        const hasDefaultFolder = this.select.checkedList.findIndex(o => o.id === o.userId)
+        if (hasDefaultFolder !== -1) {
+          this.select.checkedList.splice(hasDefaultFolder, 1)
         }
-      })
+        this.select.selectSum = '已选中' + this.select.checkedList.length + '个文件夹'
+      } else {
+        this.select.checkedList = []
+        this.select.selectSum = '全选'
+      }
+
+      if (this.search.isSearching) {
+        this.search.searchResult.forEach((o) => {
+          if (o.id !== o.userId) {
+            o.ischecked = this.select.selectAll
+            this.addOrRemoveStyle(o.id, this.select.selectAll)
+          }
+        })
+      } else {
+        this.list.forEach((o) => {
+          if (o.id !== o.userId) {
+            o.ischecked = this.select.selectAll
+            this.addOrRemoveStyle(o.id, this.select.selectAll)
+          }
+        })
+      }
     },
     // 选取单个
     handleChecked (o) {
       this.addOrRemoveStyle(o.id, o.ischecked)
-      const include = this.checkedList.findIndex(O => O.id === o.id)
+      const include = this.select.checkedList.findIndex(O => O.id === o.id)
       if (include !== -1) {
-        this.checkedList.splice(include, 1)
+        this.select.checkedList.splice(include, 1)
       } else {
-        this.checkedList.push(o)
+        this.select.checkedList.push(o)
       }
-      if (this.checkedList.length === 0) {
-        this.selectAll = false
-        this.isIndeterminate = false
-        this.selectSum = '全选'
+      if (this.select.checkedList.length === 0) {
+        this.select.selectAll = false
+        this.select.isIndeterminate = false
+        this.select.selectSum = '全选'
       } else {
-        this.isIndeterminate = true
-        this.selectSum = '已选中' + this.checkedList.length + '个文件夹'
+        this.select.isIndeterminate = true
+        this.select.selectSum = '已选中' + this.select.checkedList.length + '个文件夹'
       }
     },
     // 选中后修改样式以及取消选中后去除样式
@@ -277,11 +306,12 @@ export default {
     addOrRemoveStyle (id, checked) {
       let element
       if (this.search.isSearching) {
-        element = document.getElementById(`sno${id}`)
+        const ID = `SID-${id}`
+        element = document.getElementById(ID)
       } else {
-        element = document.getElementById(`no${id}`)
+        const ID = `ID-${id}`
+        element = document.getElementById(ID)
       }
-
       if (checked) {
         element.classList.add('userindex-el-card--select')
         // input[0].style.visibility = 'visible'
@@ -290,26 +320,51 @@ export default {
         // input[0].style.visibility = 'hidden'
       }
     },
-    // 选中与全选->
+    // ->
 
     route (id) {
       // 如果没有被选中的文件夹，则进行跳转
-      if (this.checkedList.length === 0) {
+      if (this.select.checkedList.length === 0) {
         this.$router.push({ path: '/user/nfolder/' + id })
       }
     },
 
-    // 分页条件查询 笔记与笔记文件夹
+    // 分页条件查询笔记与笔记文件夹(注意，查询笔记功能暂未完成，只能查询笔记文件夹)
     searchNfolderANDNote (page = 1) {
       if (this.search.searchKeyWord !== '') {
         this.search.isSearching = true
         this.pagination1.current = page
+        this.select = {
+          checkedList: [],
+          selectAll: false,
+          isIndeterminate: false,
+          selectSum: '全选'
+        }
         userApi.getNfolderANDNote(this.pagination2.current, this.pagination2.limit, this.search.searchKeyWord).then((response) => {
           this.search.searchResult = response.data.data.items
           this.pagination1.total = response.data.data.total
+          this.list.forEach((item) => {
+            item.shapes.forEach((tem) => {
+              tem.ischecked = false
+            })
+          })
+          // 清除选中样式
+          this.list.forEach((o) => {
+            this.addOrRemoveStyle(o.id, false)
+          })
         })
       } else {
+        // 清除选中样式
+        this.search.searchResult.forEach((o) => {
+          this.addOrRemoveStyle(o.id, false)
+        })
         this.search.isSearching = false
+        this.select = {
+          checkedList: [],
+          selectAll: false,
+          isIndeterminate: false,
+          selectSum: '全选'
+        }
       }
     },
 
@@ -319,8 +374,8 @@ export default {
       this.NfolderDialog.title = title
       this.NfolderDialog.visible = true
       if (title === '编辑文件夹') {
-        this.NfolderDialog.form.nfolderName = this.checkedList[0].folderName
-        this.NfolderDialog.form.description = this.checkedList[0].folderDescription
+        this.NfolderDialog.form.nfolderName = this.select.checkedList[0].folderName
+        this.NfolderDialog.form.description = this.select.checkedList[0].folderDescription
       } else {
         this.NfolderDialog.form.nfolderName = ''
         this.NfolderDialog.form.description = ''
@@ -330,14 +385,14 @@ export default {
     submitNfolder () {
       if (this.NfolderDialog.title === '编辑文件夹') {
         const data = {
-          nfolderID: this.checkedList[0].id,
+          nfolderID: this.select.checkedList[0].id,
           folderName: this.NfolderDialog.form.nfolderName,
           description: this.NfolderDialog.form.description
         }
         userApi.alterUserNfolder(qs.stringify((data))).then((response) => {
           if (response.data.code === 20000) {
             this.getList()// 获取新的笔记文件夹列表
-            this.checkedList = []// 清空 checkedList
+            this.select.checkedList = []// 清空 checkedList
             this.NfolderDialog.visible = false
           } else {
             this.$message.error(response.data.message)
@@ -354,12 +409,12 @@ export default {
         })
       }
     },
-    // NfolderDialog ->
+    // ->
 
     // 批量删除
     deleteUserNFolder () {
       const deleteIdList = []
-      this.checkedList.forEach((o) => {
+      this.select.checkedList.forEach((o) => {
         deleteIdList.push(o.id)
       })
 
@@ -367,16 +422,17 @@ export default {
         if (response.data.code === 20000) {
           this.$message('删除成功')
           this.getList()
-          this.checkedList = []
-          this.selectAll = false
-          this.isIndeterminate = false
-          this.selectSum = '全选'
+          this.select.checkedList = []
+          this.select.selectAll = false
+          this.select.isIndeterminate = false
+          this.select.selectSum = '全选'
         } else {
           this.$message.error(response.data.message)
         }
       })
       this.deleteDialog.visible = false
     }
+
   }
 }
 </script>
